2e907b47ea687b1e4497259d7fe1afb23d7768d8
vaishu1036 <vaishvanibmvaishvani@gmail.com>
2025-03-05 12:25:19 +0530

    adding interface

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main implements Project{
+    public void hello(){
+        System.out.println("hello everyone");
+    }
+    public static void main(String[] args) {
+        Main c = new Main();
+        c.hello();
+    }
+}
+    
+
+
+    
